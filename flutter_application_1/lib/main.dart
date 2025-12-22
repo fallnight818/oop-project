b9c10ba17024ce05fbc22f86414f10eaa79d96f6
3SAJ <funnymkh80@gmail.com>
2025-12-23 00:13:19 +0700

    t1

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pixel_Adventure.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
 
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
-  Flame.device.setLandscape();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
 
   PixelAdventure game = PixelAdventure();
-  runApp(GameWidget(game: kDebugMode ? game : PixelAdventure()));
+  runApp(
+    GameWidget(game: kDebugMode ? PixelAdventure() : game),
+  );
 }
